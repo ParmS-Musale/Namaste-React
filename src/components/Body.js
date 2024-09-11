f4b2243aeae4118f286a9b8561a8509acfbe6b90
ParmS-Musale <musaleparm9541@gmail.com>
2024-09-11 17:49:33 +0530

    Day 5 Completed..!!

---
+++ src/components/Body.js
@@ -0,0 +1,33 @@
+import resList from "../utils/mockData";
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+
+const Body = () => {
+
+ // Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestraunt(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
