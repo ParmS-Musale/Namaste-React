7a0264b55b4486d80837f7d87dafbd6794b81818
ParmS-Musale <musaleparm9541@gmail.com>
2024-09-15 11:46:49 +0530

    Day 8.1 Completed ( React Life Cycle )..!!

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -42,3 +42,22 @@ class UserClass extends React.Component {
 }
 
 export default UserClass;
+
+
+/****
+ *
+ * --- MOUNTING ----
+ *
+ * Constructor (dummy)
+ * Render (dummy)
+ *      <HTML Dummy >
+ * Component Did MOunt
+ *      <API Call>
+ *      <this.setState> -> State variable is updated
+ *
+ * ---- UPDATE
+ *
+ *      render(APi data)
+ *      <HTML (new API data>)
+ *      ccomponentDid Update
+ */
